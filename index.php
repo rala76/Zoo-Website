e3ae95fa89335ba9414aa7cd26d930df3c4b82bd
Randy <71798014+rala76@users.noreply.github.com>
2022-04-03 09:07:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Zoo-Project</h1>
     <?php
 
-    echo "Connecting to SQL server..."."<br>"; 
+    echo "Connecting to Azure SQL server..."."<br>"; 
     
     // PHP Data Objects(PDO) Sample Code:
     try {
@@ -20,12 +20,12 @@
     }
 
     // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "rala", "pwd" => "Password123", "Database" => "Zoo-Project-DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $connectionInfo = array("UID" => "user1", "pwd" => "Password123", "Database" => "Zoo-Project-DB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:cosc3380projectsserver.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
     
 
-    echo "Successfully connected to SQL server!"."<br>";
+    echo "Successfully connected to Azure SQL server!"."<br>";
     echo "<br>";
 
     ?>
