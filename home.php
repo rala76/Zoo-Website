d7e974d5fcb4bcf55f63325bc04e47928761a615
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-18 11:31:16 -0500

    Fixed small syntax issue

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
+if ($_SESSION['loggedin'] != true) {
 	header('Location: index.php');
 	exit;
 }
