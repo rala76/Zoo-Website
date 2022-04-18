b82aa7db36dd114ff4584a005ec074058eab624f
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-18 11:31:32 -0500

    Removed old home page/changed some variable names

--- index.php
+++ index.php
@@ -9,54 +9,12 @@
     <div class="login">
         <h1>Login to Zoo Database</h1>
         <form action="authenticate.php" method="post">
-            <label for="username"></label>
-            <input type="text" name="username" placeholder="Username" id="username" required>
-            <label for="password"></label>
-            <input type="password" name="password" placeholder="Password" id="password" required>
+            <label for="Username"></label>
+            <input type="text" name="Username" placeholder="Username" id="Username" required>
+            <label for="Password"></label>
+            <input type="Password" name="Password" placeholder="Password" id="Password" required>
             <input type="submit" value="Login">
         </form>
     </div>
 </body>
 </html>
-
-<!-- 'Index' as homepage -->
-<!-- <!doctype html>
-<html>
-<head>
-    <link rel="stylesheet" href="/styles.css">
-    <title>Zoo-Project</title>
-</head>
-<body>
-    <div class="header"> -->
-        <!-- Hyperlink to the Employee web page -->
-        <!-- <div class="navText">
-            <a href="/Employees/Employee.php" style="text-decoration:none;color:inherit">Employees</a>
-        </div> -->
-        <!-- Hyperlink to the Customer web page -->
-        <!-- <div class="navText">
-            <a href="/Customers/Customer.php" style="text-decoration:none;color:inherit">Customers</a>
-        </div> -->
-        <!-- Hyperlink to the Products web page -->
-        <!-- <div class="navText">
-            <a href="/Products/Product.php" style="text-decoration:none;color:inherit">Products</a>
-        </div> -->
-        <!-- Hyperlink to the Stores web page -->
-        <!-- <div class="navText">
-            <a href="/Stores/Stores.php" style="text-decoration:none;color:inherit">Stores</a>
-        </div> -->
-        <!-- Hyperlink to the Events web page -->
-        <!-- <div class="navText">
-            <a href="/Events/Events.php" style="text-decoration:none;color:inherit">Events</a>
-        </div> -->
-        <!-- Hyperlink to the Animals web page -->
-        <!-- <div class="navText">
-            <a href="/Animals/Animal.php" style="text-decoration:none;color:inherit">Animals</a>
-        </div> -->
-        
-        <!-- Hyperlink to the Enclosure web page -->
-        <!-- <div class="navText">
-            <a href="/Enclosures/Enclosure.php" style="text-decoration:none;color:inherit">Enclosures</a>
-        </div>
-    </div>
-</body>
-</html> -->
