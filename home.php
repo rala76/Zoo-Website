312a6836e82d570ebcd9733196542152c3a8ee5f
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-18 09:49:24 -0500

    Fixed small syntax error

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.html');
+	header('Location: index.php');
 	exit;
 }
 ?>
