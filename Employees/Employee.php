f3782a9c95debfc2cb5408de28a467027d4039e5
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-18 12:15:31 -0500

    Added sidebar and hyperlink to employee table

--- Employees/Employee.php
+++ Employees/Employee.php
@@ -12,23 +12,24 @@
         <div>
             <hr class="sidebarLine"></hr>
             <div class="navText sidebarText">
-                <!-- Hyperlink to the Employee Insert page -->
                 <a href="/Employees/Front/insert-employee.php" style="text-decoration:none;color:inherit">Insert New Employee</a>
             </div>
             <hr class="sidebarLine"></hr>
             <div class="navText sidebarText">
-                <!-- Hyperlink to the Employee Delete page -->
                 <a href="/Employees/Front/delete-employee.php" style="text-decoration:none;color:inherit">Delete Employee</a>
             </div>
             <hr class="sidebarLine"></hr>
             <div class="navText sidebarText">
-                <!-- Hyperlink to the Employee Update page -->
                 <a href="/Employees/Front/update-employee.php" style="text-decoration:none;color:inherit">Update Employee</a>
             </div>
             <hr class="sidebarLine"></hr>
             <div class="navText sidebarText">Get Employee Information</div>
             <hr class="sidebarLine"></hr>
             <div class="navText sidebarText">Search Employees</div>
+            <hr class="sidebarLine"></hr>
+            <div class="navText sidebarText">
+                <a href="/Employees/Front/view-employee-table.php" style="text-decoration:none;color:inherit">View Employee Table</a>
+            </div>
             <hr class="sidebarLine"></hr>
         </div>
     </div>
