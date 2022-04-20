1739679cce4ddde92b26f50ae0fee688841e7349
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-20 18:30:32 -0500

    Added welcome message

--- Login/customer-home.php
+++ Login/customer-home.php
@@ -4,12 +4,11 @@
     <!-- Check session -->
     <?php include_once(__DIR__."/../check-session.php") ?>
     
-    <link rel="stylesheet" href="/Styles/styles.css">
     <title>Customer Home Page</title>
 </head>
 <body>
     <div class="header">
-        <div>Welcome, customer!</div>
+        <h>Welcome, customer!</h>
     </div>
 </body>
 </html>
