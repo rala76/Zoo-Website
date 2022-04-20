9cd42f719804f4f305d6c15ef6426d35f801bd13
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-20 18:21:44 -0500

    Created base customer-home page

---
+++ Login/customer-home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Check session -->
+    <?php include_once(__DIR__."/../check-session.php") ?>
+    
+    <link rel="stylesheet" href="/Styles/styles.css">
+    <title>Customer Home Page</title>
+</head>
+<body>
+    <div class="header">
+        <div>Welcome, customer!</div>
+    </div>
+</body>
+</html>
