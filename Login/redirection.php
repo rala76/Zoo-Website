9eae332b148cbc747e8e673b20bdbc77a249bbf7
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-21 16:26:43 -0500

    Added customer redirection page

---
+++ Login/redirection.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="/Styles/loginStyles.css">
+    <link rel="stylesheet" href="/Styles/styles.css">
+    <title>Register for Zoo Account</title>
+</head>
+<body>
+    <div class="login">
+        <h1>
+            <a href="/Login/logon.php">Customer Already Registered!</br>Navigate Back To Login?</a>
+        </h1>
+    </div>
+</body>
+</html>
