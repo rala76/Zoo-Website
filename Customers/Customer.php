90a1856d64cdd06a06f40e940b1d7edd249202bf
moosestafa <60297592+moosestafa@users.noreply.github.com>
2022-04-23 12:11:49 -0600

    Add files via upload

--- Customers/Customer.php
+++ Customers/Customer.php
@@ -1,28 +1,28 @@
 <!-- 'Customer' => 'Insert/Delete/Update/Get/Search' -->
 <!doctype html>
 <html>
+
 <head>
     <!-- Check session -->
-    <?php include_once(__DIR__."/../check-session.php") ?>
+    <?php include_once(__DIR__ . "/../check-session.php") ?>
 
     <!-- Include index page (header) -->
-    <?php include(__DIR__."/../Login/admin-home.php") ?>
+    <?php include(__DIR__ . "/../Login/admin-home.php") ?>
 
     <title>Customer</title>
 </head>
+
 <body>
     <div class="sidebar">
         <div>
-            <hr class="sidebarLine"></hr>
-            <a href="/Customers/Front/insert-customers.php" class="navText sidebarText">Insert New Customer</a>
-            <hr class="sidebarLine"></hr>
-            <a href="/Customers/Front/delete-customers.php" class="navText sidebarText">Delete Customer</a>
-            <hr class="sidebarLine"></hr>
-            <a href="/Customers/Front/update-customers.php" class="navText sidebarText">Update Customer</a>
-            <hr class="sidebarLine"></hr>
+
+            <hr class="sidebarLine">
+            </hr>
             <a href="/Customers/Front/search-customers.php" class="navText sidebarText">Search Customers</a>
-            <hr class="sidebarLine"></hr>
+            <hr class="sidebarLine">
+            </hr>
         </div>
     </div>
 </body>
+
 </html>
