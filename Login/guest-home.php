9e5eafc826ea3877c9d0590f2a31c5f95980d2d2
Mayssam Kalajo <mayssam.kalajo@hcss.com>
2022-04-23 08:15:19 -0500

    Landing page WIP

--- Login/guest-home.php
+++ Login/guest-home.php
@@ -4,11 +4,23 @@
     <!-- Check session -->
     <?php include_once(__DIR__."/../check-session.php") ?>
     
+    <link rel="stylesheet" href="/Styles/styles.css">
+    <link rel="stylesheet" href="/Styles/guestStyles.css">
     <title>Guest Home Page</title>
 </head>
 <body>
     <div class="header">
-        <h>Welcome, guest!</h>
+        <a href="/Guest/Animals.php" class="navText">Animals</a>
+        <a href="/Guest/Shops.php" class="navText">Shops</a>
+        <a href="/Guest/Events.php" class="navText">Events</a>
+        <a href="/Guest/Tickets.php" class="navText">Tickets</a>    
+
+        <div class="banner" style="margin-top:15vh">
+            <img src="/Guest/Images/Animals.png">
+        </div>
+        <div class="banner"></div>
+        <div class="banner"></div>
+        <div class="banner"></div>
     </div>
 </body>
 </html>
