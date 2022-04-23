b9a3633ed30611504c5858f314f08a5f26d8f5cb
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2022-04-23 17:01:46 -0500

    updated UI

--- Login/logon.php
+++ Login/logon.php
@@ -1,24 +1,34 @@
+
 <!doctype html>
 <html>
+	
 <head>
-    <link rel="stylesheet" href="/Styles/loginStyles.css">
+    <link rel="stylesheet" href="loginStyles.css">
     <link rel="stylesheet" href="/Styles/styles.css">
     <title>Login to Zoo Database</title>
 </head>
+	
+<div class="header">
+	<a href="https://zoo-project.azurewebsites.net/Login/logon.php">
+	<img src="https://logos-world.net/wp-content/uploads/2021/03/San-Diego-Zoo-Logo.png" alt="logo" />
+	</a>
+</div>
+	
 <body>
+	<h1 class="zoo">Uma Zoo</h1>
     <div class="login">
-        <h1>Login to Zoo Database</h1>
+        <h1>Login</h1>
         <form action="authenticate.php" method="post">
             <label for="Username"></label>
             <input type="text" name="Username" placeholder="Username" id="Username" required>
             <label for="Password"></label>
             <input type="Password" name="Password" placeholder="Password" id="Password" required>
-            <input type="submit" value="Login">
+            <input type="submit" value="Submit">
         </form>
     </div>
-    <div class="login">
+        <div class="login">
         <h1>
-            <a href="/Login/registration.php">Create an account?</a>
+            <a href="/Login/registration.php">Create account</a>
         </h1>
     </div>
 </body>
