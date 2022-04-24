1777f8c8781ba888e3cf276925965f2539ded0a7
moosestafa <60297592+moosestafa@users.noreply.github.com>
2022-04-24 15:29:02 -0600

    Add files via upload

---
+++ Animals/Front/process-animals.php
@@ -0,0 +1,149 @@
+<?php
+// Connect to Microsoft Azure SQL Database
+include(__DIR__ . "/../../connect-sql.php");
+
+// Insert Animal based on Insert form
+if (isset($_POST["Animal-insert-2"])) {
+    $Name = $_POST["Animal-Name"];
+    $Species = $_POST["Animal-Species"];
+    $Date_Of_Birth = $_POST["Animal-date-of-birth"];
+    $Gender = $_POST["Animal-gender"];
+    $Animal_ID = $_POST["Animal-Animal_ID"];
+    $Enclosure_ID = $_POST["Animal-Enclosure_ID"];
+
+    // Create insert query
+    $sql_1 = "INSERT INTO [dbo].[Animal_Data] 
+        ([Animal_Name]
+        ,[Species]
+        ,[Date_Of_Birth]
+        ,[Gender]
+        ,[Animal_ID]
+        ,[Enclosure_ID])
+        VALUES 
+        (?,?,?,?,?,?)";
+
+    // Parameters of insert query
+    $params = array(
+        $Name, $Species, $Date_Of_Birth, $Gender, $Animal_ID, $Enclosure_ID
+    );
+
+    $stmt_1 = sqlsrv_query($conn, $sql_1, $params);
+    if ($stmt_1 == false) {
+        echo "<script> alert('Failed to Insert Animal'); </script>";
+    } else {
+        echo "<script> alert('Successfully Inserted Animal'); </script>";
+    }
+}
+
+// Get input values for Edit form
+if (isset($_POST["Animal-edit-1"])) {
+    $Animal_ID = $_POST["emp-edit-ID-1"];
+
+    // Info of Animal to be updated
+    $sql_2 = "SELECT * FROM [dbo].[Animal_Data] 
+        WHERE [Animal_ID]={$Animal_ID}";
+
+    $stmt_2 = sqlsrv_query($conn, $sql_2);
+    if ($stmt_2 == false) {
+        echo "<script> alert('Failed to Find Animal'); </script>";
+    } else if (sqlsrv_has_rows($stmt_2) <= 0) {
+        echo "<script> alert('Animal Not Found'); </script>";
+    }
+
+    // Fetch Animal from Animal_Data
+    $data = sqlsrv_fetch_array($stmt_2, SQLSRV_FETCH_ASSOC);
+}
+
+// Update Animal based on Edit form
+if (isset($_POST["Animal-edit-2"])) {
+    $Animal_ID = $_POST["emp-edit-ID-2"];
+
+    $Name = $_POST["Animal-Name"];
+    $Species = $_POST["Animal-Species"];
+    $Date_Of_Birth = $_POST["Animal-date-of-birth"];
+    $Gender = $_POST["Animal-gender"];
+    $Animal_ID = $_POST["Animal-department-name"];
+    $Enclosure_ID = $_POST["Animal-department-id"];
+
+    $sql_3 = "UPDATE [dbo].[Animal_Data] 
+        SET [Animal_Name] = '$Name'
+        ,[Species] = '$Species'
+        ,[Date_Of_Birth] = '$Date_Of_Birth'
+        ,[Gender] = '$Gender'
+        ,[Animal_ID] = '$Animal_ID'
+        ,[Enclosure_ID] = '$Enclosure_ID'
+        WHERE [Animal_ID]='$Animal_ID'";
+
+    $stmt_3 = sqlsrv_query($conn, $sql_3);
+    if ($stmt_3 == false || sqlsrv_rows_affected($stmt_3) <= 0) {
+        echo "<script> alert('Failed to Update Animal'); </script>";
+    } else {
+        echo "<script> alert('Successfully Updated Animal'); </script>";
+    }
+
+    // Info of updated Animal
+    $sql_4 = "SELECT * FROM [dbo].[Animal_Data] 
+        WHERE [Animal_ID]={$Animal_ID}";
+    $stmt_4 = sqlsrv_query($conn, $sql_4);
+
+    // Fetch Animal from Animal_Data
+    $data = sqlsrv_fetch_array($stmt_4, SQLSRV_FETCH_ASSOC);
+}
+
+// Delete Animal
+if (isset($_POST["Animal-delete"])) {
+    $Animal_ID = $_POST["emp-delete-ID"];
+
+    $sql_5 = "DELETE FROM [dbo].[Animal_Data]
+        WHERE [Animal_ID]={$Animal_ID}";
+
+    $stmt_5 = sqlsrv_query($conn, $sql_5);
+    if ($stmt_5 == false) {
+        echo "<script> alert('Failed to Delete Animal'); </script>";
+    } else if (sqlsrv_rows_affected($stmt_5) <= 0) {
+        echo "<script> alert('Animal Not Found'); </script>";
+    } else {
+        echo "<script> alert('Successfully Deleted Animal'); </script>";
+    }
+}
+
+// Select query based on Sort/Order by value
+if (!isset($_POST["Animal-search-submit"])) {
+    $_POST["Animal-sortBy"] = "Animal ID";
+    $_POST["Animal-orderBy"] = "Ascending";
+
+    $sql_6 = "SELECT *
+        FROM [dbo].[Animal_Data] 
+        ORDER BY Animal_ID ASC ";
+} else {
+    // Get Sort By value based on input
+    if ($_POST["Animal-sortBy"] == "Animal ID") {
+        $Sort_By = "Animal_ID";
+    } else if ($_POST["Animal-sortBy"] == "Name") {
+        $Sort_By = "Name";
+    } else if ($_POST["Animal-sortBy"] == "Species") {
+        $Sort_By = "Species";
+    } else if ($_POST["Animal-sortBy"] == "Date of Birth") {
+        $Sort_By = "Date_Of_Birth";
+    } else if ($_POST["Animal-sortBy"] == "Enclosure ID") {
+        $Sort_By = "Enclosure_ID";
+    } else {
+        $Sort_By = "Gender";
+    }
+
+    // Create select query based on
+    if ($_POST["Animal-orderBy"] == "Ascending") {
+        $sql_6 = "SELECT *
+            FROM [dbo].[Animal_Data] 
+            ORDER BY '$Sort_By' ASC ";
+    } else {
+        $sql_6 = "SELECT *
+            FROM [dbo].[Animal_Data] 
+            ORDER BY '$Sort_By' DESC ";
+    }
+}
+
+$stmt_6 = sqlsrv_query($conn, $sql_6);
+if ($stmt_6 == false) {
+    echo "<script> alert('Failed to load table') </script>";
+}
--- Animals/Front/search-animals.php
+++ Animals/Front/search-animals.php
@@ -1,119 +1,233 @@
+<?php
+// Include default Animal page
+include(__DIR__ . "/../Animal.php");
+
+// Include process code for forms & tables
+include(__DIR__ . "/process-animals.php");
+?>
+
 <!doctype html>
 <html>
-<head>
-    <!-- Include default Animal page -->
-    <?php include(__DIR__."/../Animal.php"); ?>
 
+<head>
     <title>Search Animals</title>
+    <link rel="stylesheet" href="/Styles/popupStyles.css">
+
+    <!-- Include JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
+
 <body>
     <div class="form-base">
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <!-- Sort table results -->
+        <form method="post">
             <!-- Dropdown list for Sort By -->
-            <label class="required-input-label">Sort By:</label><br>
-            <select name="animal-sortBy" class="dropdown-control" required>
+            <label class="input-label">Sort By:</label><br>
+            <select name="Animal-sortBy" class="dropdown-control">
                 <!-- Default option -->
-                <option value="<?php echo isset($_POST['animal-sortBy']) ? $_POST['animal-sortBy'] : '' ?>" hidden>
-                    <?php echo isset($_POST['animal-sortBy']) ? $_POST['animal-sortBy'] : 'Select an Option' ?>
+                <option value="<?php echo isset($_POST['Animal-sortBy']) ? $_POST['Animal-sortBy'] : '' ?>" hidden>
+                    <?php echo isset($_POST['Animal-sortBy']) ? $_POST['Animal-sortBy'] : 'Select an Option' ?>
                 </option>
 
                 <option value="Animal ID">Animal ID</option>
-                <option value="Animal Name">Animal Name</option>
+                <option value="Name">Name</option>
                 <option value="Species">Species</option>
-                <option value="Date Of Birth">Date Of Birth</option>
-                <option value="Gender">Gender</option>
+                <option value="Date of Birth">Date of Birth</option>
+                <option value="Animal ID">Animal ID</option>
                 <option value="Enclosure ID">Enclosure ID</option>
+
             </select><br>
 
             <!-- Dropdown list for Order By -->
-            <label class="required-input-label">Order By:</label><br>
-            <select name="animal-orderBy" class="dropdown-control" required>
+            <label class="input-label">Order By:</label><br>
+            <select name="Animal-orderBy" class="dropdown-control">
                 <!-- Default option -->
-                <option value="<?php echo isset($_POST['animal-orderBy']) ? $_POST['animal-orderBy'] : '' ?>" hidden>
-                    <?php echo isset($_POST['animal-orderBy']) ? $_POST['animal-orderBy'] : 'Select an Option' ?>
+                <option value="<?php echo isset($_POST['Animal-orderBy']) ? $_POST['Animal-orderBy'] : '' ?>" hidden>
+                    <?php echo isset($_POST['Animal-orderBy']) ? $_POST['Animal-orderBy'] : 'Select an Option' ?>
                 </option>
 
                 <option value="Ascending">Ascending</option>
                 <option value="Descending">Descending</option>
             </select><br>
 
-            <button name="animal-search-submit" type="submit" class="form-submit">Submit</button>
+            <button name="Animal-search-submit" type="submit" class="form-submit">Submit</button>
         </form>
 
-        <?php
-        // Connect to Microsoft Azure SQL Database
-        include(__DIR__."/../../connect-sql.php");
+        <!-- ==================================================================================================== -->
+        <!-- ==================================================================================================== -->
 
-        // Select query based on Sort/Order by
-        if (isset($_POST["animal-search-submit"])) {
-            // Get Sort By value based on input
-            if ($_POST["animal-sortBy"] == "Animal ID") {
-                $Sort_By = "Animal_ID";
-            }
-            else if ($_POST["animal-sortBy"] == "Animal Name") {
-                $Sort_By = "Animal_Name";
-            }
-            else if ($_POST["animal-sortBy"] == "Species") {
-                $Sort_By = "Species";
-            }
-            else if ($_POST["animal-sortBy"] == "Date Of Birth") {
-                $Sort_By = "Date_Of_Birth";
-            }
-            else if ($_POST["animal-sortBy"] == "Gender") {
-                $Sort_By = "Gender";
-            }
-            else {
-                $Sort_By = "Enclosure_ID";
-            }
-            
+        <!-- Break row -->
+        <div class='break-row'></div>
+        <div class='break-row'></div>
+        <div class='break-row'></div>
+        <div class='break-row'></div>
+        <!-- Insert Animal -->
+        <form action="#insert-popup" method="post" style="margin-bottom: -10%">
+            <button name='Animal-insert-1' id='Animal-insert' type='submit' class="button button-insert">Insert Animal</button>
+        </form>
 
-            // Create select query based on
-            if ($_POST["animal-orderBy"] == "Ascending") {
-                $sql = "SELECT [Animal_ID], [Animal_Name], [Species], [Date_Of_Birth]
-                    , [Gender], [Enclosure_ID]
-                    FROM [dbo].[Animal_Data] 
-                    ORDER BY '$Sort_By' ASC ";
-            }
-            else {
-                $sql = "SELECT [Animal_ID], [Animal_Name], [Species], [Date_Of_Birth]
-                    , [Gender], [Enclosure_ID]
-                    FROM [dbo].[Animal_Data] 
-                    ORDER BY '$Sort_By' DESC ";
-            }
+        <div class='break-row'></div>
+        <!-- ==================================================================================================== -->
+        <!-- ==================================================================================================== -->
 
-            $stmt = sqlsrv_query($conn, $sql);
+        <!-- Display Animal as table -->
+        <div>
+            <label class='form-control'></label><br>
+            <table class='styled-table'>
+                <tr>
+                    <th>Animal ID</th>
+                    <th>Name</th>
+                    <th>Species</th>
+                    <th>Date of Birth</th>
+                    <th>Gender</th>
+                    <th>Animal ID</th>
+                    <th>Enclosure ID</th>
+                    <th colspan="2">Action</th>
+                </tr>
 
-            // Break row
-            echo "<div class='break-row'></div>";
+                <!-- Fetch rows from Animal_Data -->
+                <?php while ($row = sqlsrv_fetch_array($stmt_6, SQLSRV_FETCH_ASSOC)) : ?>
+                    <tr>
+                        <td><?php echo $row["Animal_ID"] ?></td>
+                        <td><?php echo $row["Name"] ?></td>
+                        <td><?php echo $row["Species"] ?></td>
+                        <td><?php echo $row["Date_Of_Birth"]->format('Y-m-d') ?></td>
+                        <td><?php echo $row["Gender"] ?></td>
+                        <td><?php echo $row["Animal_ID"] ?></td>
+                        <td><?php echo $row["Animal_ID"] ?></td>
+                        <td>
+                            <form action="#edit-popup" method="post">
+                                <input name="emp-edit-ID-1" type="number" value="<?php echo $row['Animal_ID'] ?>" hidden>
 
-            // Display Animal as table
-            echo "<div>";
-            echo "<label class='form-control'></label><br>";
-            echo "<table>";
-                echo "<tr>";
-                    echo "<th>Animal ID</th>";
-                    echo "<th>Animal Name</th>";
-                    echo "<th>Species</th>";
-                    echo "<th>Date Of Birth</th>";
-                    echo "<th>Gender</th>";
-                    echo "<th>Enclosure ID</th>";
-                echo "</tr>";
+                                <button name='Animal-edit-1' id='Animal-edit-1' type='submit' class="button button-edit">Edit</button>
+                            </form>
+                        </td>
+                        <td>
+                            <form action="" method="post">
+                                <input name="emp-delete-ID" type="number" value="<?php echo $row['Animal_ID'] ?>" hidden>
 
-                // Fetch rows from Employee_Data
-                while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-                    echo "<tr>";
-                    echo "<td>" . $row["Animal_ID"] . "</td>";
-                    echo "<td>" . $row["Animal_Name"] . "</td>";
-                    echo "<td>" . $row["Species"] . "</td>";
-                    echo "<td>" . $row["Date_Of_Birth"]->format('Y-m-d') . "</td>";
-                    echo "<td>" . $row["Gender"] . "</td>";
-                    echo "<td>" . $row["Enclosure_ID"] . "</td>";
-                    echo "</tr>";
-                }
-            echo "</table>";
-            echo "</div>";
-        }
-        ?>
+                                <button name='Animal-delete' id='Animal-delete' type='submit' class="button button-delete">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
+
+        <!-- ==================================================================================================== -->
+        <!-- ==================================================================================================== -->
+
+        <!-- Edit form -->
+        <div id="edit-popup" class="overlay">
+            <div class="popup popup-form">
+                <h2>Edit Animal</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content content-form">
+                    <form action="" method="post">
+                        <!-- Hidden input to get Animal ID -->
+                        <input name="emp-edit-ID-2" type="number" value="<?php echo $data['Animal_ID'] ?>" hidden>
+
+                        <label class="required-input-label">Name</label><br>
+                        <input name="Animal-Name" type="text" class="form-control" required value="<?php echo isset($data['Animal_Name']) ? $data['Animal_Name'] : '' ?>">
+                        <br>
+
+                        <label class="required-input-label">Species</label><br>
+                        <input name="Animal-Species" type="text" class="form-control" required value="<?php echo isset($data['Species']) ? $data['Species'] : '' ?>">
+                        <br>
+
+                        <label class="required-input-label">Date of Birth (YYYY-MM-DD)</label><br>
+                        <input name="Animal-date-of-birth" type="text" class="form-control" required value="<?php echo isset($data['Date_Of_Birth']) ? $data['Date_Of_Birth']->format('Y-m-d') : '' ?>">
+                        <br>
+
+                        <!-- Dropdown list for Gender -->
+                        <label class="required-input-label">Gender</label><br>
+                        <select name="Animal-gender" class="dropdown-control" required>
+                            <!-- Default option -->
+                            <option value="<?php echo isset($data['Gender']) ? $data['Gender'] : '' ?>" hidden>
+                                <?php echo isset($data['Gender']) ? $data['Gender'] : 'Select an Option' ?>
+                            </option>
+
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select><br>
+
+                        <label class="required-input-label">Animal ID</label><br>
+                        <input name="Animal-Animal_ID" type="number" min="1" class="form-control" required value="<?php echo isset($data['Animal_ID']) ? $data['Animal_ID'] : '' ?>">
+                        <br>
+
+                        <label class="input-label">Enclosure ID</label><br>
+                        <input name="Animal-Enclosure_ID" type="number" min="0" step="0.01" class="form-control" placeholder="0" value="<?php echo isset($data['Enclosure_ID']) ? $data['Enclosure_ID'] : '' ?>">
+                        <br>
+
+
+
+                        <button name="Animal-edit-2" type="submit" class="form-submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- ==================================================================================================== -->
+        <!-- ==================================================================================================== -->
+
+        <!-- Insert form -->
+        <div id="insert-popup" class="overlay">
+            <div class="popup popup-form">
+                <h2>Insert Animal</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content content-form">
+                    <form action="" method="post">
+                        <label class="required-input-label">Name</label><br>
+                        <input name="Animal-Name" type="text" class="form-control" required value="<?php echo isset($_POST['Animal-Name']) ? $_POST['Animal-Name'] : '' ?>">
+                        <br>
+
+                        <label class="required-input-label">Species</label><br>
+                        <input name="Animal-Species" type="text" class="form-control" required value="<?php echo isset($_POST['Animal-Species']) ? $_POST['Animal-Species'] : '' ?>">
+                        <br>
+
+                        <label class="required-input-label">Date of Birth (YYYY-MM-DD)</label><br>
+                        <input name="Animal-date-of-birth" type="text" class="form-control" required value="<?php echo isset($_POST['Animal-date-of-birth']) ? $_POST['Animal-date-of-birth'] : '' ?>">
+                        <br>
+
+                        <!-- Dropdown list for Gender -->
+                        <label class="required-input-label">Gender</label><br>
+                        <select name="Animal-gender" class="dropdown-control" required>
+                            <!-- Default option -->
+                            <option value="<?php echo isset($_POST['Animal-gender']) ? $_POST['Animal-gender'] : '' ?>" hidden>
+                                <?php echo isset($_POST['Animal-gender']) ? $_POST['Animal-gender'] : 'Select an Option' ?>
+                            </option>
+
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select><br>
+
+                        <label class="required-input-label">Animal ID</label><br>
+                        <input name="Animal-Animal_ID" type="number" min="1" class="form-control" placeholder="NULL" required value="<?php echo isset($_POST['Animal-Animal_ID']) ? $_POST['Animal-Animal_ID'] : '' ?>">
+                        <br>
+
+                        <label class="input-label">Enclosure ID</label><br>
+                        <input name="Animal-Enclosure_ID" type="number" min="0" step="0.01" class="form-control" placeholder="0" value="<?php echo isset($_POST['Animal-Enclosure_ID']) ? $_POST['Animal-Enclosure_ID'] : '' ?>">
+                        <br>
+
+
+
+                        <button name="Animal-insert-2" type="submit" class="form-submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    <!-- Popup alert when edit button clicked (TESTING JQuery) -->
+    <!-- <script>
+        $(document).ready(function() {
+            $(document).on('click', '#Animal-edit-1', function() {
+                alert("Edit button clicked");
+            });
+        });
+    </script> -->
+
 </body>
+
 </html>
