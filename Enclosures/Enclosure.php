9ba88156c5fcc77e54627a197ff21e81b623dae4
moosestafa <60297592+moosestafa@users.noreply.github.com>
2022-04-23 18:31:32 -0600

    Add files via upload

--- Enclosures/Enclosure.php
+++ Enclosures/Enclosure.php
@@ -1,28 +1,28 @@
 <!-- 'Enclosure' => 'Insert/Delete/Update/Get/Search' -->
 <!doctype html>
 <html>
+
 <head>
     <!-- Check session -->
-    <?php include_once(__DIR__."/../check-session.php") ?>
+    <?php include_once(__DIR__ . "/../check-session.php") ?>
 
     <!-- Include index page (header) -->
-    <?php include(__DIR__."/../Login/admin-home.php") ?>
+    <?php include(__DIR__ . "/../Login/admin-home.php") ?>
 
     <title>Enclosure</title>
 </head>
+
 <body>
     <div class="sidebar">
         <div>
-            <hr class="sidebarLine"></hr>
-            <a href="/Enclosures/Front/insert-enclosures.php" class="navText sidebarText">Insert New Enclosure</a>
-            <hr class="sidebarLine"></hr>
-            <a href="/Enclosures/Front/delete-enclosures.php" class="navText sidebarText">Delete Enclosure</a>
-            <hr class="sidebarLine"></hr>
-            <a href="/Enclosures/Front/update-enclosures.php" class="navText sidebarText">Update Enclosure</a>
-            <hr class="sidebarLine"></hr>
+
+            <hr class="sidebarLine">
+            </hr>
             <a href="/Enclosures/Front/search-enclosures.php" class="navText sidebarText">Search Enclosures</a>
-            <hr class="sidebarLine"></hr>
+            <hr class="sidebarLine">
+            </hr>
         </div>
     </div>
 </body>
+
 </html>
